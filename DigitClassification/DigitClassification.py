8cf36a39550feedb8a4b43b1acfc6927527dde6c
Anastasiia Babenko <ababenko@my.bridgeport.edu>
2020-03-31 11:42:12 -0400

    I wrote a skeleton for training NN, mentioning all needed functions and implemented forward propagation

--- DigitClassification/DigitClassification.py
+++ DigitClassification/DigitClassification.py
@@ -2,26 +2,46 @@ import os
 import sys
 import cv2
 import numpy as np
+from NN import NN 
 from sklearn.utils import shuffle
-import NN 
-#network=NN()
-train = np.empty((1000,28,28),dtype='float64')
-trainY = np.zeros((1000,10,1))
-test = np.empty((10000,28,28),dtype='float64')
-testY = np.zeros((10000,10,1))
 
-# Load in the images
-i = 0
-for filename in os.listdir('C:/Users/anast/Documents/Deep Learning/Data/Training1000/'):
-    y = int(filename[0])
-    trainY[i,y] = 1.0
-    train[i] = cv2.imread('C:/Users/anast/Documents/Deep Learning/Data/Training1000/{0}'.format(filename),0)/255.0 # 0 flag stands for greyscale; for color, use 1
-    i = i + 1
-i = 0 # read test data
-for filename in os.listdir('C:/Users/anast/Documents/Deep Learning/Data/Test10000/'):
-    y = int(filename[0])
-    testY[i,y] = 1.0
-    test[i] = cv2.imread('C:/Users/anast/Documents/Deep Learning/Data/Test10000/{0}'.format(filename),0)/255.0
-    i = i + 1
-trainX = train.reshape(train.shape[0],train.shape[1]*train.shape[2],1)
-testX = test.reshape(test.shape[0],test.shape[1]*test.shape[2],1)
+
+def main():
+    '''check how shuffle works
+    X=np.array([[[0],[0],[3],[4],[3],[4],[3],[0],[0]],[[0],[0],[5],[4],[3],[4],[3],[0],[0]],[[0],[0],[6],[4],[3],[4],[3],[0],[0]],[[0],[0],[2],[4],[3],[4],[3],[0],[0]],[[0],[0],[9],[4],[3],[4],[3],[0],[0]]])
+    print(X.shape)
+    Y=np.zeros((5,10,1))
+    Y[0,3]=1
+    Y[1,5]=1
+    Y[2,6]=1
+    Y[3,2]=1
+    Y[4,9]=1
+    for i in range(5):
+        X,Y=shuffle(X,Y)
+        print(X)'''
+    train = np.empty((1000,28,28),dtype='float64')
+    trainY = np.zeros((1000,10,1))
+    test = np.empty((10000,28,28),dtype='float64')
+    testY = np.zeros((10000,10,1))
+
+    # Load in the images
+    i = 0
+    for filename in os.listdir('C:/Users/anast/Documents/Deep Learning/Data/Training1000/'):
+        y = int(filename[0])
+        trainY[i,y] = 1.0
+        train[i] = cv2.imread('C:/Users/anast/Documents/Deep Learning/Data/Training1000/{0}'.format(filename),0)/255.0 # 0 flag stands for greyscale; for color, use 1
+        i = i + 1
+    i = 0 # read test data
+    for filename in os.listdir('C:/Users/anast/Documents/Deep Learning/Data/Test10000/'):
+        y = int(filename[0])
+        testY[i,y] = 1.0
+        test[i] = cv2.imread('C:/Users/anast/Documents/Deep Learning/Data/Test10000/{0}'.format(filename),0)/255.0
+        i = i + 1
+    trainX = train.reshape(train.shape[0],train.shape[1]*train.shape[2],1)
+    testX = test.reshape(test.shape[0],test.shape[1]*test.shape[2],1)
+    #network=NN()
+    NN.trainNN(trainX,trainY)
+
+if __name__ == "__main__":
+    sys.exit(int(main() or 0))
+
--- DigitClassification/NN.py
+++ DigitClassification/NN.py
@@ -1,14 +1,55 @@
 import numpy as np
+from sklearn.utils import shuffle
+
 class NN(object):
        
-    def trainNN(X,Y):
+    def sigmoid(x):
+        return 1/(1+np.exp(-x))
+
+    def forwardProp(w1,b1,w2,b2,xi):
+        #w1 is 100x784, b1 is 100x1, xi is 784x1, w2 is 10x100, b2 is 10x1
+        s1=w1@xi+b1 #100x1
+        a1=NN.sigmoid(s1) #100x1
+        s2=w2@a1+b2 #10x1
+        a2=NN.sigmoid(s2)
+        return s1,a1,s2,a2
+
+    def computeLoss(actualOut,expectedOut):
+        #should return total loss of current model 
+        x=0
+
+    def derivativeOfLoss(t):
+        #should return partial derivative of t with respect to loss
+        x=0
+
+    def trainNN(X,Y,alpha=0.1): #X is (1000x784x1), Y is (1000x10x1)
         w1 = np.random.uniform(-0.12,0.12,(100,784))
         b1 = np.random.uniform(-0.12,0.12,(100,1))
         w2 = np.random.uniform(-0.12,0.12,(10,100))
         b2 = np.random.uniform(-0.12,0.12,(10,1))
-        epochs=100
-        #for i in range(epochs):
-        return w1,b1,w2,b2
+        #regularization parameter:lambda
+        regLambda=0.01
+        epochsNum=1 #100
+        samplesNum=5#X.shape[0]
+        newX=X
+        newY=Y
+
+        for j in range(epochsNum):
+            newX,newY=shuffle(newX,newY)
+            for i in range(samplesNum):
+                xi=X[i]#same as X[i,:]
+                #compute forward pass
+                s1,a1,s2,a2 = NN.forwardProp(w1,b1,w2,b2,xi)
+                '''loss=computeLoss(a2,Y[i])
+                backpropagation: updating weigths and biases
+                w1=w1-alpha(derivativeOfLoss(w1)- regLambda*w1)
+                b1=b1-alpha(derivativeOfLoss(b1)- regLambda*b1)
+                w2=w2-alpha(derivativeOfLoss(w2)- regLambda*w2)
+                b2=b2-alpha(derivativeOfLoss(b2)- regLambda*b2)'''
+
+        return w1,b1,w2,b2    
+
+
     
 
 
